2c9f4b2d2a3948d13d6050687da64c4cb8a49769
Sean O'Connor <soc1641@icloud.com>
2025-11-13 22:48:22 -0600

    added routes for backend algorithm execution and env variable retrieval

--- backend/server.py
+++ backend/server.py
@@ -4,12 +4,32 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+user_api_key = None
+user_base_url = None
+user_model_name = None
+
 @app.route('/howdyworld/', methods=['GET'])
-def Howdy_World():
+def howdy_horld():
     
     return jsonify({"message": "Howdy World!"})
 
+@app.route('/env/', methods=['POST'])
+def set_env_variables():
+    global user_api_key, user_base_url, user_model_name
 
+    data = request.get_json()
+
+    user_api_key = data.get("api_key")
+    user_base_url = data.get("base_url")
+    user_model_name = data.get("model_name")
+
+    return jsonify({"status": "Environment variables received."})
+
+@app.route('/algorithm/', methods=['GET'])
+def run_algorithm():
+    if user_api_key == None or user_base_url == None or user_model_name == None:
+        return jsonify({"error": "Environment variables not set"}), 400
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
