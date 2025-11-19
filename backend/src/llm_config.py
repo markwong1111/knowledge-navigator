d132117db9fec2bbff9e4d3954b8f5c26b313303
Sean O'Connor <soc1641@icloud.com>
2025-11-18 23:29:58 -0600

    fixed llm file

--- backend/src/llm_config.py
+++ backend/src/llm_config.py
@@ -10,26 +10,12 @@ def get_llm(temperature=0, model_name=None, api_base=None, api_key=None):
     """
     Returns an LLM instance
     """
-    OPENAI_API_KEY="AIzaSyBDectKJ04QzSIB9MM5LfjganhHGtTz8oM"
-
-    LLM_MODEL_NAME = model_name if model_name else os.getenv("LLM_MODEL_NAME")
-    LM_STUDIO_API_BASE = api_base if api_base else os.getenv("LM_STUDIO_API_BASE")
-    OPENAI_API_KEY = api_key if api_key else os.getenv("OPENAI_API_KEY")
-
-
 
         # openai_api_base=lm_studio_api_base,
     llm_instance = ChatOpenAI(
             base_url=api_base,
             api_key=api_key,
             model=model_name,
-            temperature=temperature
-    )
-
-    llm_instance = ChatOpenAI(
-            base_url="https://generativelanguage.googleapis.com/v1beta",
-            api_key="AIzaSyBDectKJ04QzSIB9MM5LfjganhHGtTz8oM",
-            model="gemini-2.0-flash",
             temperature=temperature
     )
 
